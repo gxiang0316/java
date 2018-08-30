50b9ed935ddb6b4e1812d382cfa839d4f5962229
guixiang <gxiang0208@126.com>
2018-08-30 11:28:42 +0800

    springboot2.0.4整合activiti modeler5.22.0

---
+++ src/main/java/com/example/activiti/ActivitimavenApplication.java
@@ -0,0 +1,14 @@
+package com.example.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.activiti.spring.boot.SecurityAutoConfiguration.class})
+public class ActivitimavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitimavenApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/activiti/config/ActitytiDataSourceConfig.java
@@ -0,0 +1,43 @@
+package com.example.activiti.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.activiti.spring.boot.AbstractProcessEngineAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class ActitytiDataSourceConfig extends AbstractProcessEngineAutoConfiguration {
+
+
+
+
+    @Bean(name = "activitiDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.activiti")
+    public DataSource activitiDataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(activitiDataSource());
+    }
+
+    @Bean
+    public SpringProcessEngineConfiguration springProcessEngineConfiguration() {
+        SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+        configuration.setDataSource(activitiDataSource());
+        configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        configuration.setJobExecutorActivate(true);
+        configuration.setTransactionManager(transactionManager());
+        return configuration;
+    }
+
+}
