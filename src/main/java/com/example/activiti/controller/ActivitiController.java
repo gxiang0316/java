8b51ffb7a191d5c66ae4ae8fe06496b9bab584ed
guixiang <gxiang0208@126.com>
2018-09-04 11:48:25 +0800

    springboot2.0.4整合activiti modeler5.22.0，增加页面，导入，导出

--- src/main/java/com/example/activiti/controller/ActivitiController.java
+++ src/main/java/com/example/activiti/controller/ActivitiController.java
@@ -1,17 +1,33 @@
 package com.example.activiti.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.editor.constants.ModelDataJsonConstants;
+import org.activiti.editor.language.json.converter.BpmnJsonConverter;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.activiti.explorer.ExplorerApp;
+import org.activiti.explorer.ViewManager;
+import org.activiti.explorer.util.XmlUtil;
+import org.activiti.bpmn.model.BpmnModel;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 @RestController
 @RequestMapping("/activiti")
@@ -50,6 +66,147 @@ public class ActivitiController {
             response.sendRedirect(request.getContextPath() + "/modeler.html?modelId=" + modelData.getId());
         } catch (Exception e) {
             System.out.println("创建模型失败：");
+        }
+    }
+
+    /**
+     * 创建模型
+     */
+//    @RequestMapping("/import")
+//    public ModelAndView import()
+//    {
+//        try
+//        {
+//            return new ModelAndView()
+//        }
+//        catch (Exception e)
+//        {
+//        }
+//    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public void deployUploadedFile( @RequestParam("uploadfile") MultipartFile uploadfile , HttpServletRequest request , HttpServletResponse response)
+    {
+        InputStreamReader in = null;
+        try
+        {
+            try
+            {
+                boolean validFile = false;
+                String fileName = uploadfile.getOriginalFilename();
+                if (fileName.endsWith(".bpmn20.xml") || fileName.endsWith(".bpmn"))
+                {
+                    validFile = true;
+                    XMLInputFactory xif = XmlUtil.createSafeXmlInputFactory();
+                    in = new InputStreamReader(new ByteArrayInputStream(uploadfile.getBytes()), "UTF-8");
+                    XMLStreamReader xtr = xif.createXMLStreamReader(in);
+                    BpmnModel bpmnModel = new BpmnXMLConverter().convertToBpmnModel(xtr);
+                    if (bpmnModel.getMainProcess() == null || bpmnModel.getMainProcess().getId() == null)
+                    {
+//                        notificationManager.showErrorNotification(Messages.MODEL_IMPORT_FAILED,
+//                                i18nManager.getMessage(Messages.MODEL_IMPORT_INVALID_BPMN_EXPLANATION));
+                        System.out.println("err1");
+                    }
+                    else
+                    {
+                        if (bpmnModel.getLocationMap().isEmpty())
+                        {
+//                            notificationManager.showErrorNotification(Messages.MODEL_IMPORT_INVALID_BPMNDI,
+//                                    i18nManager.getMessage(Messages.MODEL_IMPORT_INVALID_BPMNDI_EXPLANATION));
+                            System.out.println("err2");
+                        }
+                        else
+                        {
+                            String processName = null;
+                            if (StringUtils.isNotEmpty(bpmnModel.getMainProcess().getName()))
+                            {
+                                processName = bpmnModel.getMainProcess().getName();
+                            }
+                            else
+                            {
+                                processName = bpmnModel.getMainProcess().getId();
+                            }
+                            Model modelData;
+                            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+                            RepositoryService repositoryService = processEngine.getRepositoryService();
+                            modelData = repositoryService.newModel();
+                            ObjectNode modelObjectNode = new ObjectMapper().createObjectNode();
+                            modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, processName);
+                            modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
+                            modelData.setMetaInfo(modelObjectNode.toString());
+                            modelData.setName(processName);
+                            repositoryService.saveModel(modelData);
+                            BpmnJsonConverter jsonConverter = new BpmnJsonConverter();
+                            ObjectNode editorNode = jsonConverter.convertToJson(bpmnModel);
+                            repositoryService.addModelEditorSource(modelData.getId(), editorNode.toString().getBytes("utf-8"));
+                            response.sendRedirect(request.getContextPath() + "/modeler.html?modelId=" + modelData.getId());
+                        }
+                    }
+                }
+                else
+                {
+//                    notificationManager.showErrorNotification(Messages.MODEL_IMPORT_INVALID_FILE,
+//                            i18nManager.getMessage(Messages.MODEL_IMPORT_INVALID_FILE_EXPLANATION));
+                    System.out.println("err3");
+                }
+            }
+            catch (Exception e)
+            {
+                String errorMsg = e.getMessage().replace(System.getProperty("line.separator"), "<br/>");
+//                notificationManager.showErrorNotification(Messages.MODEL_IMPORT_FAILED, errorMsg);
+                System.out.println("err4");
+            }
+        }
+        finally
+        {
+            if (in != null)
+            {
+                try
+                {
+                    in.close();
+                }
+                catch (IOException e)
+                {
+//                    notificationManager.showErrorNotification("Server-side error", e.getMessage());
+                    System.out.println("err5");
+                }
+            }
+        }
+    }
+
+    @RequestMapping(value = "export/{modelId}")
+    public void export(@PathVariable("modelId") String modelId, HttpServletResponse response)
+    {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        try {
+            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+            RepositoryService repositoryService = processEngine.getRepositoryService();
+            Model modelData = repositoryService.getModel(modelId);
+            BpmnJsonConverter jsonConverter = new BpmnJsonConverter();
+            //获取节点信息
+            byte[] arg0 = repositoryService.getModelEditorSource(modelData.getId());
+            JsonNode editorNode = new ObjectMapper().readTree(arg0);
+            //将节点信息转换为xml
+            BpmnModel bpmnModel = jsonConverter.convertToBpmnModel(editorNode);
+            BpmnXMLConverter xmlConverter = new BpmnXMLConverter();
+            byte[] bpmnBytes = xmlConverter.convertToXML(bpmnModel);
+
+            ByteArrayInputStream in = new ByteArrayInputStream(bpmnBytes);
+            IOUtils.copy(in, response.getOutputStream());
+//                String filename = bpmnModel.getMainProcess().getId() + ".bpmn20.xml";
+            String filename = modelData.getName() + ".bpmn20.xml";
+            response.setHeader("Content-Disposition", "attachment; filename=" + java.net.URLEncoder.encode(filename, "UTF-8"));
+            response.flushBuffer();
+        } catch (Exception e) {
+            PrintWriter out = null;
+            try {
+                out = response.getWriter();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            out.write("未找到对应数据");
+            e.printStackTrace();
         }
     }
 }
